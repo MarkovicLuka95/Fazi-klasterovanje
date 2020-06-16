82306d4daf3b93da288ac08df1bcdf0a4053af59
tamaraRado <lukavrl@gmail.com>
2020-06-16 23:00:19 +0200

    finalna verzija

--- Gustafson-Kessel.py
+++ Gustafson-Kessel.py
@@ -6,147 +6,137 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
+def G_K(broj_klastera=3, m=2, max_iter=30, siroviPodaci=0):
+    podaci = siroviPodaci.to_numpy()
 
-
-def G_K(podaci, broj_klastera=3, m=2, max_iter=30,siroviPodaci=0):
-
-    #ne znam sta treba ro da mi bude, pise ||A|| = ro = konst, ali to me zbunjuje ako se A izracunava kasnije
-    # negde kazu da se A random inicijalizuje...
     ro = np.ones(broj_klastera)
-    redovi = podaci.shape[0]
-    trenutni = 0
+    brRedova = podaci.shape[0]
+    brIter = 0
     fig = plt.figure(figsize=(15, 9))
     plt_ind = 1
 
-    for i in range(2,broj_klastera+1):
-        funkcijaPripadnosti = inicijalizujMatricuPripadnosti(redovi, i)
-        while trenutni < max_iter:
-            centroidi = racunanjeCentara(i, redovi, m, podaci, funkcijaPripadnosti)
-            A = matricaKovarijanse(redovi,i,funkcijaPripadnosti,podaci,centroidi,ro)
-            dist = distance(i,redovi,centroidi,podaci,A)
-            funkcijaPripadnosti = AzuriranjeMatricePripadnosti(funkcijaPripadnosti, dist, m, redovi, i)
-            trenutni += 1
-        trenutni = 0;
+    for i in range(2, broj_klastera + 1):
+        matricaPripadnosti = inicijalizujMatricuPripadnosti(brRedova, i)
+        while brIter < max_iter:
+            centroidi = racunanjeCentara(i, brRedova, m, podaci, matricaPripadnosti)
+            A = matricaA(brRedova, i, matricaPripadnosti, podaci, centroidi, ro)
+            rastojanje = racunanjeRastojanja(i, brRedova, centroidi, podaci, A)
+            matricaPripadnosti = AzuriranjeMatricePripadnosti(matricaPripadnosti, rastojanje, m, brRedova, i)
+            brIter += 1
+        brIter = 0;
 
         fig.add_subplot(3, 3, plt_ind)
-        for j in range (1,len(podaci)):
-            plt.scatter(siroviPodaci["x"][j],siroviPodaci["y"][j], color = mesanjeBoja(i,funkcijaPripadnosti[j]), marker='o')
+        for j in range(1, len(podaci)):
+            plt.scatter(siroviPodaci["x"][j], siroviPodaci["y"][j], color=mesanjeBoja(i, matricaPripadnosti[j]),
+                        marker='o')
 
         for j in range(0, i):
             plt.scatter(centroidi.transpose()[0][j], centroidi.transpose()[1][j], color="black", marker='x')
         plt_ind += 1;
     plt.show()
 
-def mesanjeBoja(broj_klastera,red):
-    color = np.array([0.0,0.0,0.0])
+
+def mesanjeBoja(broj_klastera, redMatricePripadnosti):
+    colors = np.array(
+        [[1.0, 0.0, 0.0], [0.0, 0.0, 1.0], [0.0, 1.0, 0.0], [1.0, 1.0, 0.0], [0.0, 1.0, 1.0], [1.0, 0.0, 1.0],
+         [0, 0, 0], [0, 1, 0]])
+    color = np.array([0.0, 0.0, 0.0])
     for i in range(broj_klastera):
-        color +=colors[i]*red[i]
+        color += colors[i] * redMatricePripadnosti[i]
     return matplotlib.colors.to_hex(color)
 
 
-
-def distance(broj_klastera, red, centroidi, podaci,A):
-    dist=np.empty(shape=(broj_klastera,red))
+def racunanjeRastojanja(broj_klastera, brRedova, centroidi, podaci, A):
+    rastojanje = np.empty(shape=(broj_klastera, brRedova))
     for i in range(broj_klastera):
-        for k in range (red):
-            diff = podaci[k]-centroidi[i]
-            diff.reshape(1,2)
+        for k in range(brRedova):
+            diff = podaci[k] - centroidi[i]
+            diff.reshape(1, 2)
             ret = np.matmul(diff, A[i])
-            ret = np.matmul(ret,diff)
-            dist[i,k]=ret
-    return dist
+            ret = np.matmul(ret, diff)
+            rastojanje[i, k] = ret
+    return rastojanje
 
-def matricaA(F,i, ro):
-    A= pow(ro[i]*np.linalg.det(F), 1/m) * np.linalg.inv(F)
+
+def matricaUA(F, i, ro):
+    A = pow(ro[i] * np.linalg.det(F), 1 / m) * np.linalg.inv(F)
     return A
 
-def matricaKovarijanse(red,broj_klastera,funkcijaPripadnosti,podaci,centroidi,ro):
 
-    f=np.empty(shape=(broj_klastera,broj_klastera))
-    A=[]
+def matricaA(brRedova, broj_klastera, matricaPripadnosti, podaci, centroidi, ro):
+    f = np.empty(shape=(broj_klastera, broj_klastera))
+    A = []
     for i in range(broj_klastera):
         brojilac = 0;
         imenilac = 0;
-        for k in range(red):
-            a1=(podaci[k] - centroidi[i])
-            a1=a1.reshape(1,2)
-            b=a1.reshape(-1,1)
-            e = np.matmul(b,a1)
-            brojilac += (funkcijaPripadnosti[k, i] ** m) * e
-            imenilac += funkcijaPripadnosti[k, i] ** m
-        f=brojilac/imenilac
-        a=matricaA(f,i,ro)
-        A.append(a)
+        for k in range(brRedova):
+            a1 = (podaci[k] - centroidi[i])
+            a1 = a1.reshape(1, 2)
+            b = a1.reshape(-1, 1)
+            e = np.matmul(b, a1)
+            brojilac += (matricaPripadnosti[k, i] ** m) * e
+            imenilac += matricaPripadnosti[k, i] ** m
+        f = brojilac / imenilac
+        Ai = matricaUA(f, i, ro)
+        A.append(Ai)
 
     return A
 
 
-def inicijalizujMatricuPripadnosti(red,broj_klastera):
-    funkcijaPripadnosti=np.empty(shape=(red,broj_klastera))
-    for i in range(red):
+def inicijalizujMatricuPripadnosti(brRedova, broj_klastera):
+#radom marica sa sumom 1
+    matricaPripadnosti = np.empty(shape=(brRedova, broj_klastera))
+    for i in range(brRedova):
         random_num_list = [random.random() for i in range(broj_klastera)]
         summation = sum(random_num_list)
-        temp_list = [x/summation for x in random_num_list]
-        funkcijaPripadnosti[i]=temp_list;
-    return funkcijaPripadnosti
+        temp_list = [x / summation for x in random_num_list]
+        matricaPripadnosti[i] = temp_list;
+    return matricaPripadnosti
 
-# #ovo izgleda ne treba....
-# def initCentersandA(red,broj_klastera):
-#     centroidi=np.empty(shape=(broj_klastera,2))
-#     for i in range(broj_klastera):
-#         random_num_list = [random.random() for i in range(2)]
-#         centroidi[i]=random_num_list;
-#     return (centroidi)
 
-def racunanjeCentara(broj_klastera, red, m,data,funkcijaPripadnosti):
-    centroidi = np.empty(shape=(broj_klastera,2))
-    broj_kordinata=2;
+def racunanjeCentara(broj_klastera, brRedova, m, data, matricaPripadnosti):
+    centroidi = np.empty(shape=(broj_klastera, 2))
+    broj_kordinata = 2;
 
     for i in range(broj_klastera):
-        for j in range (broj_kordinata):
-            brojilac=0;
-            imenilac=0;
-            for k in range(red):
-                brojilac+=data[k][j]*(funkcijaPripadnosti[k,i]**m)
-                imenilac+=funkcijaPripadnosti[k,i]**m
-            centroidi[i,j]=brojilac/imenilac
+        for j in range(broj_kordinata):
+            brojilac = 0;
+            imenilac = 0;
+            for k in range(brRedova):
+                brojilac += data[k][j] * (matricaPripadnosti[k, i] ** m)
+                imenilac += matricaPripadnosti[k, i] ** m
+            centroidi[i, j] = brojilac / imenilac
     return centroidi
 
 
-
-
-def AzuriranjeMatricePripadnosti(funkcijaPripadnosti, dist,m,red,broj_klastera):
-    p = float(1/(m-1))
+def AzuriranjeMatricePripadnosti(matricaPripadnosti, rastojanje, m, brRedova, broj_klastera):
+    p = float(1 / (m - 1))
 
     for i in range(broj_klastera):
-        for k in range(red):
-            suma =  (dist[i][k]**(p))*sum(([1/((dist[j][k])**(p)) for j in range(broj_klastera)]))
-            funkcijaPripadnosti[k,i]=1/suma
+        for k in range(brRedova):
+            suma = (rastojanje[i][k] ** (p)) * sum(([1 / ((rastojanje[j][k]) ** (p)) for j in range(broj_klastera)]))
+            matricaPripadnosti[k, i] = 1 / suma
+
+    return matricaPripadnosti
 
 
-    return funkcijaPripadnosti
+# pocetak main fukcije
 
-
-#pocetak meina
-
-colors = np.array([[1.0,0.0,0.0],[0.0,0.0,1.0],[0.0,1.0,0.0],[1.0,1.0,0.0],[0.0,1.0,1.0],[1.0,0.0,1.0],[0,0,0],[0,1,0]])
-broj_klastera=8
-m=2
+broj_klastera = 8
+m = 2
 max_iter = 20
-#df = pd.read_csv('data.csv')
-#df = pd.read_csv('data2.csv')
+# df = pd.read_csv('data.csv')
+# df = pd.read_csv('data2.csv')
 df = pd.read_csv('data3.csv')
-df = df.sample(300)
+df = df.sample(200)
 features = df.columns[0:2]
-df=df[['x','y']]
+df = df[['x', 'y']]
 
 scaler = prep.MinMaxScaler().fit(df)
 x = pd.DataFrame(scaler.transform(df[features]))
 x.columns = features
-siroviPodaci=x
-x=x.to_numpy()
 
-G_K(x,broj_klastera, m, max_iter,siroviPodaci);
+G_K(broj_klastera, m, max_iter, x);
 
 
 
--- fcm.py
+++ fcm.py
@@ -10,54 +10,55 @@ import matplotlib
 
 def fcm(podaci, broj_klastera=3, m=2, max_iter=30):
     #     #1)zadamo broj klastera, inicijalizujemo funkciju pripadnosti random
-    #     funkcijaPripadnosti=inicijalizujMatricuPripadnosti(redovi,broj_klastera)
+    #     matricaPripadnosti=inicijalizujMatricuPripadnosti(brRedova,broj_klastera)
     #     #2)racunamo centre
-    #     centroidi=racunanjeCentara(broj_klastera,redovi, m,podaci,funkcijaPripadnosti)
+    #     centroidi=racunanjeCentara(broj_klastera,brRedova, m,podaci,matricaPripadnosti)
     #     #3)popravljamo funkciju pripadnosti
-    #     funkcijaPripadnosti=AzuriranjeMatricePripadnosti(funkcijaPripadnosti,centroidi,podaci,m,redovi,broj_klastera)
+    #     matricaPripadnosti=AzuriranjeMatricePripadnosti(matricaPripadnosti,centroidi,podaci,m,brRedova,broj_klastera)
 
-    redovi = podaci.shape[0]
-    trenutni = 0
+    brRedova = podaci.shape[0]
+    brIter = 0
 
     fig = plt.figure(figsize=(15, 9))
     plt_ind = 1
 
     for i in range(2,broj_klastera+1):
-        funkcijaPripadnosti = inicijalizujMatricuPripadnosti(redovi, i)
-        while trenutni < max_iter:
-            centroidi = racunanjeCentara(i, redovi, m, podaci, funkcijaPripadnosti)
-            funkcijaPripadnosti = AzuriranjeMatricePripadnosti(funkcijaPripadnosti, centroidi, podaci, m, redovi, i)
-            trenutni += 1
-        trenutni = 0
+        matricaPripadnosti = inicijalizujMatricuPripadnosti(brRedova, i)
+        while brIter < max_iter:
+            centroidi = racunanjeCentara(i, brRedova, m, podaci, matricaPripadnosti)
+            matricaPripadnosti = AzuriranjeMatricePripadnosti(matricaPripadnosti, centroidi, podaci, m, brRedova, i)
+            brIter += 1
+        brIter = 0
 
         fig.add_subplot(3, 3, plt_ind)
 
         for j in range (1,len(podaci)):
-            plt.scatter(podaci["x"][j],podaci["y"][j], color = mesanjeBoja(i,funkcijaPripadnosti[j]), marker='o')
+            plt.scatter(podaci["x"][j],podaci["y"][j], color = mesanjeBoja(i,matricaPripadnosti[j]), marker='o')
         for j in range(0, i):
             plt.scatter(centroidi.transpose()[0][j], centroidi.transpose()[1][j], color="black", marker='x')
         plt_ind +=1;
     plt.show()
 
-    print(funkcijaPripadnosti)
+    print(matricaPripadnosti)
 
-def mesanjeBoja(broj_klastera,red):
+def mesanjeBoja(broj_klastera,redUMatriciPripadsti):
+    colors = np.array([[1.0,0.0,0.0],[0.0,0.0,1.0],[0.0,1.0,0.0],[1.0,1.0,0.0],[0.0,1.0,1.0],[1.0,0.0,1.0],[0,0,0],[0,1,0]])
     color = np.array([0.0,0.0,0.0])
     for i in range(broj_klastera):
-        color +=colors[i]*red[i]
+        color +=colors[i]*redUMatriciPripadsti[i]
     return matplotlib.colors.to_hex(color)
 
-def inicijalizujMatricuPripadnosti(red,broj_klastera):
-    funkcijaPripadnosti=np.empty(shape=(red,broj_klastera))
+def inicijalizujMatricuPripadnosti(brRedova,broj_klastera):
+    matricaPripadnosti=np.empty(shape=(brRedova,broj_klastera))
 
-    for i in range(red):
+    for i in range(brRedova):
         random_num_list = [random.random() for i in range(broj_klastera)]
         summation = sum(random_num_list)
         temp_list = [x/summation for x in random_num_list]
-        funkcijaPripadnosti[i]=temp_list;
-    return funkcijaPripadnosti
+        matricaPripadnosti[i]=temp_list;
+    return matricaPripadnosti
 
-def racunanjeCentara(broj_klastera, red, m,podaci,funkcijaPripadnosti):
+def racunanjeCentara(broj_klastera, brRedova, m,podaci,matricaPripadnosti):
     centroidi = np.empty(shape=(broj_klastera,2))
     broj_kordinata=2;
 
@@ -65,35 +66,34 @@ def racunanjeCentara(broj_klastera, red, m,podaci,funkcijaPripadnosti):
         for j in range (broj_kordinata):
             brojilac=0;
             imenilac=0;
-            for k in range(red):
-                brojilac+=podaci.iloc[k][j]*(funkcijaPripadnosti[k,i]**m)
-                imenilac+=funkcijaPripadnosti[k,i]**m
+            for k in range(brRedova):
+                brojilac+=podaci.iloc[k][j]*(matricaPripadnosti[k,i]**m)
+                imenilac+=matricaPripadnosti[k,i]**m
             centroidi[i,j]=brojilac/imenilac
     return centroidi
 
-def distance(dist, broj_klastera, red, centroidi, podaci):
+def racunanjeRastojanja(rastojanje, broj_klastera, brRedova, centroidi, podaci):
     for i in range(broj_klastera):
-        for k in range(red):
-            dist[i][k] = np.linalg.norm(centroidi[i] - podaci.iloc[k])
-    return dist
+        for k in range(brRedova):
+            rastojanje[i][k] = np.linalg.norm(centroidi[i] - podaci.iloc[k])
+    return rastojanje
 
 
-def AzuriranjeMatricePripadnosti(funkcijaPripadnosti, centroidi,podaci,m,red,broj_klastera):
+def AzuriranjeMatricePripadnosti(matricaPripadnosti, centroidi,podaci,m,brRedova,broj_klastera):
     p = float(2/(m-1))
-    dist=np.empty(shape=(broj_klastera,red))
-    dist=distance(dist,broj_klastera,red,centroidi,podaci)
+    rastojanje=np.empty(shape=(broj_klastera,brRedova))
+    rastojanje=racunanjeRastojanja(rastojanje,broj_klastera,brRedova,centroidi,podaci)
 
     for i in range(broj_klastera):
-        for k in range(red):
-            suma =  (dist[i][k]**(p))*sum(([1/((dist[j][k])**(p)) for j in range(broj_klastera)]))
-            funkcijaPripadnosti[k,i]=1/suma
+        for k in range(brRedova):
+            suma =  (rastojanje[i][k]**(p))*sum(([1/((rastojanje[j][k])**(p)) for j in range(broj_klastera)]))
+            matricaPripadnosti[k,i]=1/suma
 
-    return funkcijaPripadnosti
+    return matricaPripadnosti
 
 
 #pocetak meina
 
-colors = np.array([[1.0,0.0,0.0],[0.0,0.0,1.0],[0.0,1.0,0.0],[1.0,1.0,0.0],[0.0,1.0,1.0],[1.0,0.0,1.0],[0,0,0],[0,1,0]])
 broj_klastera=8
 m=2
 max_iter = 10
@@ -109,6 +109,7 @@ x = pd.DataFrame(scaler.transform(df1))
 x.columns = features
 
 fcm(x,broj_klastera, m, max_iter)
+
 
 
 
